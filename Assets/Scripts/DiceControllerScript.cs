0445f237eef961610825a1ad1e16c635da326e7f
Cian Dicker-Hughes <cian@redkic.co.uk>
2025-10-13 20:16:01 +0100

    add input to the dice roller
    users can select dices but can't roll the dice yet

--- Assets/Scripts/DiceControllerScript.cs
+++ Assets/Scripts/DiceControllerScript.cs
@@ -2,17 +2,40 @@ using UnityEngine;
 
 public class DiceControllerScript : MonoBehaviour
 {
+        private int selectedDiceType = 0;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+     // Call this from your dice button's OnClick event, passing the dice type (e.g., 4, 6, 8, etc.)
+    public void OnDiceButtonClicked(int diceType)
+    {
+        selectedDiceType = diceType;
+        Debug.Log($"Dice button clicked! D{diceType} was selected.");
+    }
+
+    // Call this from your Roll Dice button's OnClick event
+    public void OnRollDiceButtonClicked()
+    {
+        if (selectedDiceType == 0)
+        {
+            Debug.Log("No dice selected!");
+            return;
+        }
+        int result = RollDice(selectedDiceType, 1, 0); // Rolls 1 dice, modifier 0
+        Debug.Log($"Rolled D{selectedDiceType}: Result = {result}");
+    }
+
 
     // Method to roll dice
     public int RollDice(int diceType, int diceCount, int modifier)
