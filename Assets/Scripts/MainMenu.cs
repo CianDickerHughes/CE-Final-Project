81792eae4b89af53338b4b27725fbe4e7c9c6abe
Eoin Ocathasaigh <g00417466@atu.ie>
2025-10-21 09:18:47 +0100

    Added some simple explanation comments for the navigation script for future use cases

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    /*
+    EXPLANATION OF CODE FOR FUTURE USE
+    This script will only help us with navigation between the various pages
+    To add new navigation features:
+    - Add the scene you want to travel to, to the build path
+    - Add a new method here for moving to the scene in the actual project
+    - Change the name in the LoadSceneAsync() method to the scene you want e.g. "SampleScene"
+    - Go back to the project & hook up the button to this method & drag across whatever controller/manager we may be using
+    */
 
     //Method we will use to actually switch from main menu to playing the game
     public void PlayGame()
@@ -18,7 +27,7 @@ public class MainMenu : MonoBehaviour
 
     public void CreateGame()
     {
-        //This is the exact same as the one above - it only switches to the character creator screen
+        //This is the exact same as the one above - it only switches to the "Create Game" scene
         SceneManager.LoadSceneAsync("GameScene");
     }
     
