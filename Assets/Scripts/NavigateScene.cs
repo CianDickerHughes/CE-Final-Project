25e3c7371f41609c571520f2508494b2deb0c048
Eoin Ocathasaigh <g00417466@atu.ie>
2025-11-11 18:07:52 +0000

    Beginning Detailed Functionality Addition
    Fleshing out the campaign and character creation functionality by adding more "steps" in the process to handle the various options/routes we need to deal with. Starting with proper implementation of characters

--- Assets/Scripts/NavigateScene.cs
+++ Assets/Scripts/NavigateScene.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class MainMenu : MonoBehaviour
+public class NavigateScene : MonoBehaviour
 {
     /*
     EXPLANATION OF CODE FOR FUTURE USE
