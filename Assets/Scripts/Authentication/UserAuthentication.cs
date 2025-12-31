5d117409b15599bc972ee8454ec6ea07c6e8e372
Eoin Ocathasaigh <g00417466@atu.ie>
2025-12-31 15:03:39 +0000

    Changed some scripting to do with the authentication
    Changed scripting for panel so now it wont pop up if the user is already signed in

--- Assets/Scripts/Authentication/UserAuthentication.cs
+++ Assets/Scripts/Authentication/UserAuthentication.cs
@@ -23,14 +23,15 @@ public class UserAuthentication : MonoBehaviour
     
     void Start()
     {
-        // If already signed in, hide panel immediately
-        /*
+        //If already signed in, hide panel immediately
+        //This can happen if returning from another scene
+        //This is where things can get a bit tricky - in edit mode the SessionManager might persist which is useful for testing but prevents this from popping up again
+        //Not a problem just something to be aware of
         if (SessionManager.Instance != null && SessionManager.Instance.IsLoggedIn)
         {
             panel.SetActive(false);
             return;
         }
-        */
         // Show the panel on start
         panel.SetActive(true);
         // Load and display existing users
