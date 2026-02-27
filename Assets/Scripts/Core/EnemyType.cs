c14b46649e17d0f8f9a06e3663097bab3b8b365e
Eoin Ocathasaigh <g00417466@atu.ie>
2026-02-27 12:09:01 +0000

    Updated more details for the DM fight area and removed problematic enemy typing
    Now a proper view pops up for fighting the DM and the option for "Dungeon Master" no longer appears in the enemy AI section. It shouldnt be there anyway since that difficulty option should only be available in the actual DM fight

--- Assets/Scripts/Core/EnemyType.cs
+++ Assets/Scripts/Core/EnemyType.cs
@@ -4,7 +4,5 @@ public enum EnemyType
     //Manual - The DM is manually controlling this enemy themselves
     Manual,
     //Boss/Adaptive - Enemy is controlled by the adaptive enemy AI behaviour - learns/has actions informed by the specialised AI model in the system
-    Adaptive,
-    //Dungeon Master - Potential Highest difficulty enemy type - adaptive and can counter instantly when adapting
-    DungeonMaster
+    Adaptive
 }
