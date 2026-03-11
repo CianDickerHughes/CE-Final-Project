b3268a49e27f98a96c3eba3eb9cf2dac515d2ddd
Eoin Ocathasaigh <g00417466@atu.ie>
2026-03-11 20:26:02 +0000

    Added in useful links for the DM to use during gameplay, allows traversal to external websites for help
    Buttons now available to provide the dm with readily available information relating to items, classes, monsters etc. Done in both management and gameplay scene

---
+++ Assets/Scripts/UI/LinkResourceNav.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+//Bascially this script is meant to use the behavior of navigating to external links/web pages outside the game itself
+//Mainly to be used by the DM
+public class LinkResourceNav : MonoBehaviour
+{
+    //Method to link to a specified URL
+    public void OpenLink(string specificURL)
+    {
+        Application.OpenURL("https://5e.tools/" + specificURL);
+    }
+    
+}
