97fff1b62e0ca8b2831d1da28d026755c44d8ebf
Eoin Ocathasaigh <g00417466@atu.ie>
2026-03-29 12:31:04 +0100

    Added more details for music playing across scenes and the option to change what music is played
    2 scripts now exist for choosing what music is playing and if it persists across scenes etc

---
+++ Assets/Scripts/UI/MusicManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e1cb534c873e20b6522837c6d29077e6636afbe616233a8b86255de2b8c111a
+size 2718
---
+++ Assets/Scripts/UI/SceneMusic.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cd606d8608eab4105507841fe423cb0662b58bacfbe179b10fdb9aa48ed73a53
+size 664
