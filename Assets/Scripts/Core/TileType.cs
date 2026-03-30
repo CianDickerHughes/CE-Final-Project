3e8f6dde017155449ffb93e3ea889900983354ed
Eoin Ocathasaigh <g00417466@atu.ie>
2026-03-30 19:42:34 +0100

    Forgot to fully update the script to change from difficult terrain to lava

--- Assets/Scripts/Core/TileType.cs
+++ Assets/Scripts/Core/TileType.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c50e11cd1f7a93598a35f876b2daeb351d57105a064cb594792ffbeeb2b5d464
-size 6476
+oid sha256:2fd6810900886c08eae63a0d4fca4a64ef06472378735645e8f6522bee548706
+size 6456
