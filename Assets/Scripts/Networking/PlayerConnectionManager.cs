4b590fb8a0920252f2cdd775969c30c8bedfa50a
Cian Dicker-Hughes <cian@redkic.co.uk>
2026-03-31 23:20:34 +0100

    updated the kicking system, send client back to Campigns scenes with a message

--- Assets/Scripts/Networking/PlayerConnectionManager.cs
+++ Assets/Scripts/Networking/PlayerConnectionManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80dea8eb5b34d64a918f3d99c8cdb853750d01964f86e64dbd124d1a65f9e92d
-size 34729
+oid sha256:4002d096fb5cbae285502802c42c708aa58dcd1ae096cb10e1a7b4fc48b41d15
+size 35233
---
+++ Assets/Scripts/UI/KickNotification.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7f8b11c6d19f624a7d23aa01578ac104d0c2f6283234271110ce1e4bdd4f27a0
+size 1231
