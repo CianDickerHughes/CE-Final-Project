02b1f84980bf38f1d4e99df669c7d9cf43793b47
Cian Dicker-Hughes <cian@redkic.co.uk>
2026-03-31 22:45:33 +0100

    add a setting page for the music

--- Assets/Scripts/UI/MusicManager.cs
+++ Assets/Scripts/UI/MusicManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e1cb534c873e20b6522837c6d29077e6636afbe616233a8b86255de2b8c111a
-size 2718
+oid sha256:fac1ac7f4e10ea4857a59bc6f8a4df20ac4a94b7144351ee2d2e69c72d4f0f0d
+size 3523
---
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4dda6e33efc850f48562d4376fb7dff6acf2d42942f06bae3a72e6aa1af959dd
+size 952
